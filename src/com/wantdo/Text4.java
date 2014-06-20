3ee9f51b49658da29b7d4ca1d05321d10dd56f7c
zqhfly <1762029177@qq.com>
2014-06-20 17:16:53 +0800

    Update Text4.java

--- src/com/wantdo/Text4.java
+++ src/com/wantdo/Text4.java
@@ -7,6 +7,8 @@ public class Text4 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 
 	}
 
